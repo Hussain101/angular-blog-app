2588bc31c2fc9dcfef4db8e3f76c7f6f1d03b2b2
Hussain101 <hussainsidd88@gmail.com>
2023-12-31 21:06:52 +0500

    component done

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css'] // 'styleUrls' instead of 'styleUrl'
+})
+export class UserListComponent {
+
+}
